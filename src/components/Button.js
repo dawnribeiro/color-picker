e5a4f591d41fd81165fc1ea367cb3912ecd85d1b
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-05-23 08:02:30 -0400

    deleted button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,5 @@
 import React, { Component } from 'react'
 
-class Button extends Component {
-  randomColor = () => {
-    this.setState({
-      hue: Math.ceil(Math.random() * 360),
-      saturation: Math.ceil(Math.random() * 100),
-      lightness: Math.ceil(Math.random() * 100)
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.randomColor}>Pick Random Color</button>
-      </div>
-    )
-  }
-}
+class Button extends Component {}
 
 export default Button
